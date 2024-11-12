e50b79058a44346a88b324b356c89db0cdf6184e
Mike Stepanik <mikestepanik@gmail.com>
2024-11-11 21:36:50 -0500

    Added animated characters to homepage

--- react-portfolio/src/components/AnimatedLetters/index.js
+++ react-portfolio/src/components/AnimatedLetters/index.js
@@ -1,14 +1,16 @@
 import './index.scss';
 
-const AnimatedLetters = ({ lettersClass, strArray, idx }) => {
+const AnimatedLetters = ({ letterClass, strArray, idx }) => {
 
     return (
         <span>
-            strArray.map((char, i)) => (
-                <span key={char + i} className={`${lettersClass} _ ${i + idx}`}>
-                    {char}
-                </span>
-            ))
+           {
+                strArray.map((char, i) => (
+                    <span key={char + i} className={`${letterClass} _${i + idx}`}>
+                        {char}
+                    </span>
+                ))
+            }
         </span>
     )
 
