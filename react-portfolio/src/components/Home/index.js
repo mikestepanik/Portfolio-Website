46050757521c97b444b58d0d09a4a6d8cb2a6af3
Mike Stepanik <mikestepanik@gmail.com>
2024-11-17 18:44:32 -0500

    Added hover animation to letters on homepage. Had to remove return statement in useEffect()

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,12 +1,38 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
-    const jobArray= ['w','e','b',' ','d','e','v', 'e','l','o','p','e','r', '.']
+    const jobArray= [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v', 
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r', 
+    '.'] 
+
+
+    useEffect(() => {
+        setTimeout(() => {
+           setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //         setLetterClass('text-animate-hover')
+    //     }, 4000)
+    // }, [])
 
     return(
         <div className="container home-page">
