4639e41e0d583394c2291db06c472458abf10a4b
Mike Stepanik <mikestepanik@gmail.com>
2025-02-01 17:15:50 -0500

    Added functional map to contact page using react leaflet. Added neon cursor component that is not being used currently

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -5,17 +5,21 @@ import Layout from './components/Layout'
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import NeonCursor from './components/NeonCursor';
+// import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
+// import { useEffect, useState } from 'react';
 
 
 function App() {
+
   return (
-    <>
+   <>
    <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} /> 
         <Route path= "about" element={<About />} /> 
-        <Route path= "contact" element={<Contact />} />     
-      </Route>   
+        <Route path= "contact" element={<Contact />} />    
+      </Route> 
     </Routes>
    </>
   );
--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, useMap , Marker, Popup} from 'react-leaflet'
 
 const Contact = () => {
 
@@ -21,7 +22,7 @@ const Contact = () => {
         emailjs
             .sendForm(
                 'service_zt2qnuh',
-                'template_8beuhde', 
+                'template_8beuhde',
                 refForm.current,
                 'wK-D45zeVPhoJTt1O'
 
@@ -61,33 +62,55 @@ const Contact = () => {
                                     <input type="text" name='name' placeholder='Name' required />
                                 </li>
                                 <li className='half'>
-                                    <input 
-                                        type="email" 
-                                        name='email' 
-                                        placeholder='Email' 
-                                        required 
-                                    />                                    
+                                    <input
+                                        type="email"
+                                        name='email'
+                                        placeholder='Email'
+                                        required
+                                    />
                                 </li>
                                 <li>
-                                    <input placeholder='Subject' type="text"  name='subject' required />
+                                    <input placeholder='Subject' type="text" name='subject' required />
                                 </li>
                                 <li>
-                                    <textarea 
-                                    placeholder='Message' 
-                                    name='message' 
-                                    required>
+                                    <textarea
+                                        placeholder='Message'
+                                        name='message'
+                                        required>
                                     </textarea>
                                 </li>
                                 <li>
-                                    <input type="submit" className='flat-button' value="SEND"/>
+                                    <input type="submit" className='flat-button' value="SEND" />
                                 </li>
                             </ul>
                         </form>
                     </div>
                 </div>
-            </div>
+                <div className='info-map'>
+                    Mike Stepanik,
+                    <br />
+                    United States,
+                    <br />
+                    123 Cappa Lane, MA 01030 <br />
+                    Springfield <br />
+                    <span>mikestepanik@gmail.com</span>
+                </div>
+                <div className='map-wrap' >
+                    <MapContainer center={[44.96366, 19.61045]} zoom={13} scrollWheelZoom={true}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                    <Marker position={[44.96366, 19.61045]}>
+                        <Popup>
+                            Sloba lives here, come over for a cup of coffeee. :)
+                        </Popup>
+                    </Marker>
+                    
+                    </MapContainer>
+
+                </div>
+            </div >
             <Loader type="pacman" />
-        </>)
+        </>
+    )
 }
 
 export default Contact
