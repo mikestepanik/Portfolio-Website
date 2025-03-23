567dbcb8fc2aa833807e5deb189f9ae6e6706b4b
Mike Stepanik <mikestepanik@gmail.com>
2025-03-23 16:36:05 -0400

    Working commit with personal infoormation modifications

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -7,22 +7,25 @@ import Logo from './Logo';
 import Loader from 'react-loaders';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+    const nameArray = [' ','M', 'i', 'k', 'e']
     const jobArray = [
-        'w',
-        'e',
-        'b',
-        ' ',
-        'd',
-        'e',
-        'v',
-        'e',
-        'l',
+        'C',
         'o',
+        'm',
         'p',
+        'u',
+        't',
         'e',
         'r',
-        '.']
+        ' ',
+        'E',
+        'n',
+        'g',
+        'i',
+        'n',
+        'e',
+    'e', 
+'r']
 
 
     useEffect(() => {
@@ -46,16 +49,15 @@ const Home = () => {
                         <br />
                         <span className={`${letterClass} _13`}>I</span>
                         <span className={`${letterClass} _14`}>'m</span>
-                        <img src={LogoTitle} alt="developer" />
                         <AnimatedLetters letterClass={letterClass}
                             strArray={nameArray}
                             idx={15} />
                         <br />
                         <AnimatedLetters letterClass={letterClass}
                             strArray={jobArray}
-                            idx={22} />
+                            idx={19} />
                     </h1>
-                    <h2>Frontend Developer / JavaScript Expert / Youtuber</h2>
+                    <h2>Embedded Software / C++ Developer / Rocket League Player</h2>
                     <Link to="/contact" className='flat-button'>
                         CONTATCT ME
                     </Link>
